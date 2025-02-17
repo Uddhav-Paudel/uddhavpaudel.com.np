c9d9e2cd541384a48175a260beb6a4233e28552a
Uddhav Paudel <paudeluddhav.er@gmail.com>
2025-02-17 03:14:27 +0000

    Portfolio Update

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,23 +1,28 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+const apiUrl = import.meta.env.VITE_API_URL;
 import axios from 'axios';
 
 const hitCount = ref(0);
-const cvLink = ref('');
 
 onMounted(async () => {
-    const { data } = await axios.post('http://localhost:5000/api/hit');
+    const { data } = await axios.post(`${apiUrl}/hit`);
     hitCount.value = data.count;
-
-    const cvResponse = await axios.get('http://localhost:5000/api/cv');
-    cvLink.value = cvResponse.data.cv_link;
 });
 </script>
 
 <template>
-    <div>
-        <h1 class='text-3xl font-bold underline font-manrope'>Welcome to My Portfolio</h1>
-        <p>Site Visit Count: {{ hitCount }}</p>
-        <a :href="cvLink" download><button>Download CV</button></a>
+    <div class="flex flex-row-reverse overflow-hidden bg-[url('../assets/images/logo_white.svg')] bg-contain bg-no-repeat bg-center h-screen">
+        <img src="../assets/images/daughter.jpeg" alt="Portfolio Background Image" class="mix-blend-overlay absolute size-full object-cover object-center" />
+        <a href="https://roadmap.sh/r?id=67b2602474e25b5b30f86780" target="_blank" class="place-self-start z-1">
+            <button class="border-1 px-8 py-4 bg-black hover:bg-black text-white cursor-pointer">About</button>
+        </a>
+        <a href="https://www.linkedin.com/in/uddhav-paudel" target="_blank" class="place-self-start z-1 hover:bg-blue">
+            <button class="border-1 px-8 py-4 bg-blue-500 hover:bg-blue-600 text-white cursor-pointer">
+                <FontAwesomeIcon :icon="faLinkedinIn" class="text-xl" />                
+            </button>
+        </a>
     </div>
 </template>
