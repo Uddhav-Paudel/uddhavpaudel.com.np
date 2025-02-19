f32678d01c8a01a14c90a8c922de713d8f35ea5b
Uddhav Paudel <paudeluddhav.er@gmail.com>
2025-02-19 22:09:10 +0000

    Updating the position of Menus.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -16,10 +16,10 @@ onMounted(async () => {
 <template>
     <div class="flex flex-row-reverse overflow-hidden bg-[url('../assets/images/logo_white.svg')] bg-contain bg-no-repeat bg-center h-screen">
         <img src="../assets/images/daughter.jpeg" alt="Portfolio Background Image" class="mix-blend-overlay absolute size-full object-cover object-center" />
-        <a href="https://roadmap.sh/r?id=67b2602474e25b5b30f86780" target="_blank" class="place-self-start z-1">
+        <a href="https://roadmap.sh/r?id=67b2602474e25b5b30f86780" target="_blank" class="place-self-end z-1">
             <button class="border-1 px-8 py-4 bg-black hover:bg-black text-white cursor-pointer">About</button>
         </a>
-        <a href="https://www.linkedin.com/in/uddhav-paudel" target="_blank" class="place-self-start z-1 hover:bg-blue">
+        <a href="https://www.linkedin.com/in/uddhav-paudel" target="_blank" class="place-self-end z-1 hover:bg-blue">
             <button class="border-1 px-8 py-4 bg-blue-500 hover:bg-blue-600 text-white cursor-pointer">
                 <FontAwesomeIcon :icon="faLinkedinIn" class="text-xl" />                
             </button>
