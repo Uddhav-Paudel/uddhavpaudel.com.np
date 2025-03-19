7a06fe44651d6f8d0390cbe6362db816469a4785
Uddhav Paudel <devuddhav15@gmail.com>
2025-03-19 15:34:04 +0000

    portfolio link added

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -16,8 +16,8 @@ onMounted(async () => {
 <template>
     <div class="flex flex-row-reverse overflow-hidden bg-[url('../assets/images/logo_white.svg')] bg-contain bg-no-repeat bg-center h-screen">
         <img src="../assets/images/daughter.jpeg" alt="Portfolio Background Image" class="mix-blend-overlay absolute size-full object-cover object-center" />
-        <a href="https://roadmap.sh/r?id=67b2602474e25b5b30f86780" target="_blank" class="place-self-end z-1">
-            <button class="border-1 px-8 py-4 bg-black hover:bg-black text-white cursor-pointer">About Me</button>
+        <a href="https://portfolio.uddhavpaudel.com.np" target="_blank" class="place-self-end z-1">
+            <button class="border-1 px-8 py-4 bg-black hover:bg-black text-white cursor-pointer">Portfolio</button>
         </a>
         <a href="https://www.linkedin.com/in/uddhav-paudel" target="_blank" class="place-self-end z-1 hover:bg-blue">
             <button class="border-1 px-8 py-4 bg-blue-500 hover:bg-blue-600 text-white cursor-pointer">
